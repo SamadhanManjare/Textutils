8799c4878dabc484403a996dbc50535c6e525296
SamadhanManjare <122026715+SamadhanManjare@users.noreply.github.com>
2026-01-14 19:29:35 +0530

    Tet form file

--- src/Components/Textform.js
+++ src/Components/Textform.js
@@ -1,6 +1,7 @@
 import React, {useState} from 'react'
 
 export default function Textform(props) {
+  /// Textform Start ///
   
   const handleUpClick =()=> {
     console.log('Uppercase was clicked'+ text)
@@ -24,6 +25,7 @@ export default function Textform(props) {
   }
   const[text,setText]=useState('');
   return (
+    
     <>
     <div>
       <h1>{props.heading}</h1>
@@ -35,7 +37,7 @@ export default function Textform(props) {
     <button className='btn btn-primary mx-1' onClick={handleLoClick} >convert to lowercase</button>
     <button className='btn btn-primary mx-1' onClick={handleclearClick} >Clear Text</button>
 
-        
+          
     </div>
     <div className="container mx-2">
       <h2>Your text summary</h2>
